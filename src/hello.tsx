b36aa9fd084a3f9746a8ec3bde57ba6bbdc140bd
thalles-ab <thallesmweb@gmail.com>
2017-12-11 16:14:31 -0200

    correções

--- src/hello.tsx
+++ src/hello.tsx
@@ -1,9 +1,26 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import { Editor } from "./components/Editor";
+import { Router, Route, Link, browserHistory } from 'react-router'; 
+import { Editor } from "./components/Editor"; 
+import { Diagram } from './components/Diagram'
 
-ReactDOM.render(
-    <Editor />,
-    document.getElementById("example")
+export default class App extends React.Component{
+    render(){
+        return (
+            <div className="container">
+                <div className="grid-50"><Editor /></div>
+                <div className="grid-50"><Diagram /></div>
+            </div>
+        );
+      } 
+}
+
+ReactDOM.render( 
+    <App />,
+    document.getElementById("root")
 ); 
+
+window.onbeforeunload = function() {
+    return "Dude, are you sure you want to refresh? Think of the kittens!";
+}
