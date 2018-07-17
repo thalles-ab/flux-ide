826452dba56333ccfaf5f02453ee7dc01c72c5c9
thallesmweb@gmail.com <12357951>
2018-07-17 02:08:33 -0300

    voltando

---
+++ src/model/FunctionNode.tsx
@@ -0,0 +1,22 @@
+import NodeTree from "./NodeTree"
+
+export default class FunctionNode extends NodeTree{
+    type : string; 
+    name: string;
+
+    constructor(type, name, matched?){
+        super();
+        this.type = type;
+        this.name = name;
+
+        console.log(matched);
+        if(matched){
+            var matchedS = String(matched);
+            var frist = matchedS.indexOf('(');
+            var last = matchedS.indexOf(')');
+            console.log(matchedS.substr(frist+1, last-frist-1));
+        }
+
+    }
+
+}
